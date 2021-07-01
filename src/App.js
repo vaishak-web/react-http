680924d39be0e25f74ed3b1efb581ee9094fb48a
Vaishak Nair <vaishak.web@gmail.com>
2021-07-02 01:45:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PostList from './components/PostList';
+import Postform from './components/Postform';
+
+function App() {
+  return (
+    <div className="App">
+      <Postform />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Postform.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import axios from 'axios'
+
+class Postform extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userId: "",
+      title: "",
+      body: "",
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+    })
+  }
+
+  submitHandler = e => {
+      e.preventDefault()
+      console.log(this.state)
+      axios.post("http://jsonplaceholder.typicode.com/posts", this.state)
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          posts: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        // this.setState({
+        //   errorMsg: "Error retreiving Data",
+        // });
+      });
+  }
+  render() {
+    const { userId, title, body } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          <div>
+            <input
+              type="text"
+              name="userId"
+              value={userId}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="body"
+              value={body}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Postform;
